88eb6bfd31e35674dd4638e750e8d8e0568ec932
aldotestino <aldotestino4@gmail.com>
2025-06-29 18:59:42 +0200

    fix AppOpenAPI type

--- apps/api/src/lib/types.ts
+++ apps/api/src/lib/types.ts
@@ -1,10 +1,12 @@
 import type { OpenAPIHono, RouteConfig, RouteHandler } from '@hono/zod-openapi';
+import type { BASE_PATH } from './constants';
 
 export type AppEnv = {
   // biome-ignore lint/complexity/noBannedTypes: this is a placeholder for env variables
   Bindings: {};
 };
 
-export type AppOpenAPI = OpenAPIHono<AppEnv>;
+// biome-ignore lint/complexity/noBannedTypes: this is needed for the rpc client types
+export type AppOpenAPI = OpenAPIHono<AppEnv, {}, typeof BASE_PATH>;
 
 export type AppRouteHandler<R extends RouteConfig> = RouteHandler<R, AppEnv>;
