1913132ae587b3f5e66d5195d101cf1df00fa98c
aldotestino <aldotestino4@gmail.com>
2025-07-12 19:49:52 +0200

    ci: add DATABASE_URL environment variable for test job

---
+++ apps/api/src/index.test.ts
@@ -0,0 +1,48 @@
+import { describe, expect, test } from 'bun:test';
+import { testClient } from 'hono/testing';
+import { createApp } from './lib/create-app';
+import { registerRoutes } from './routes';
+
+describe('Api tests', () => {
+  const app = registerRoutes(createApp());
+  const client = testClient(app);
+
+  let createdNoteId: number;
+
+  test('should return a list of notes', async () => {
+    const response = await client.api.notes.$get();
+    const { notes } = await response.json();
+
+    expect(response.status).toBe(200);
+    expect(notes).toBeArray();
+  });
+
+  test('should create a new note', async () => {
+    const response = await client.api.notes.$post({
+      json: {
+        title: 'Test note',
+        description: 'Test description',
+      },
+    });
+    const { id } = await response.json();
+    createdNoteId = id;
+
+    expect(response.status).toBe(201);
+    expect(id).toBeNumber();
+  });
+
+  test('verify new note is in the list', async () => {
+    const response = await client.api.notes.$get();
+    const { notes } = await response.json();
+
+    expect(notes.map((note) => note.id)).toContain(createdNoteId);
+  });
+
+  test('should delete a note', async () => {
+    const response = await client.api.notes[':id'].$delete({
+      param: { id: createdNoteId.toString() },
+    });
+
+    expect(response.status).toBe(204);
+  });
+});
