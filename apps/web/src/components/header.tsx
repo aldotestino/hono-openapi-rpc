8b79ba082af5a94f7a91bdd493908b515c8af620
aldotestino <aldotestino4@gmail.com>
2025-07-13 19:13:30 +0200

    user menu with avatar

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,37 +1,32 @@
-import { Link, useLocation, useRouter } from '@tanstack/react-router';
+import { Link, useLocation } from '@tanstack/react-router';
 import { ArrowLeftIcon } from 'lucide-react';
-import { Button, buttonVariants } from '@/components/ui/button';
+import { buttonVariants } from '@/components/ui/button';
+import UserMenu from '@/components/user-menu';
 import { authClient } from '@/lib/auth';
 
 function Header() {
   const { pathname } = useLocation();
-  const router = useRouter();
 
   const { data } = authClient.useSession();
-
-  const handleSignOut = () =>
-    authClient.signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          router.navigate({ to: '/' });
-        },
-      },
-    });
 
   return (
     <header className="flex items-center justify-between border-b p-4">
       <div className="flex items-center gap-2">
         {pathname === '/notes/new' && (
           <Link
-            className={buttonVariants({ variant: 'ghost', size: 'icon' })}
+            className={buttonVariants({
+              variant: 'ghost',
+              size: 'icon',
+              className: 'h-8 w-8',
+            })}
             to="/notes"
           >
-            <ArrowLeftIcon className="h-4 w-4" />
+            <ArrowLeftIcon />
           </Link>
         )}
         <h1 className="font-bold text-2xl">Notes</h1>
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-4">
         {pathname === '/notes' && (
           <Link
             className={buttonVariants({
@@ -44,11 +39,7 @@ function Header() {
             New Note
           </Link>
         )}
-        {data?.session.userId && (
-          <Button onClick={handleSignOut} size="sm" variant="outline">
-            Logout
-          </Button>
-        )}
+        {data?.session.userId && <UserMenu />}
       </div>
     </header>
   );
---
+++ apps/web/src/components/user-menu.tsx
@@ -0,0 +1,55 @@
+import { useRouter } from '@tanstack/react-router';
+import { LogOut } from 'lucide-react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { authClient } from '@/lib/auth';
+
+function UserMenu() {
+  const { data } = authClient.useSession();
+
+  const router = useRouter();
+  const handleSignOut = () =>
+    authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.navigate({ to: '/' });
+        },
+      },
+    });
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={data?.user.image || undefined} />
+          <AvatarFallback>{data?.user.name.charAt(0)}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>
+          <div className="flex flex-col space-y-1">
+            <p className="font-medium text-sm">{data?.user.name}</p>
+            <p className="text-muted-foreground text-xs">{data?.user.email}</p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem onClick={handleSignOut} variant="destructive">
+            <LogOut />
+            Sign Out
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default UserMenu;
